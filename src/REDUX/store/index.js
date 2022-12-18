a0aecd6179051224be7b5380f5d17d975d83cffb
Victor Ramirez <vicdeveloper83@gmail.com>
2022-12-18 13:40:38 +0000

    first commit

--- src/REDUX/store/index.js
+++ src/REDUX/store/index.js
@@ -1,10 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-
+// import { composeWithDevTools } from 'redux-devtools-extension';
+const composeEnhancers =
+	(typeof window !== 'undefined' &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+	compose;
 const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk))
 );
+
 export default store;
